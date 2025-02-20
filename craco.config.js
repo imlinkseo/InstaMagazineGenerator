630a87eccef295139ab7e80bb7efe364ef1f9280
imlinkseo <imlinkseo@gmail.com>
2025-02-20 23:21:45 +0900

    [MOD] build 설정

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,15 @@
 const CracoAlias = require("craco-alias");
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        path: require("path").resolve(__dirname, "build"), // 빌드 폴더 설정
+      };
+      return webpackConfig;
+    },
+  },
   plugins: [
     {
       plugin: CracoAlias,
