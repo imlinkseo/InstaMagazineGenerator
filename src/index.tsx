0be4215cf162fd2d0699f9c92a57fbbffc5062d4
imlinkseo <imlinkseo@gmail.com>
2025-02-25 22:59:46 +0900

    [MOD] hashrouter 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/InstaMagazineGenerator">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
